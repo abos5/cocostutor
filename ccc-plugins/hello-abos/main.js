2e73358fad5cfd1fe70226b8efa52399ecf40746
abosfreeman <abos5@outlook.com>
2016-11-28 00:26:08 +0800

    终于获取到 scene 的数据了 1. 取消自己解析 .fire 文件 2. 直接通过 IPC 从 sendToPanel 获取数据 3. 观察了获取到的数据结构, hierarchy.node.values 是绑定的结果值, 预计可以使用这里的值

--- ccc-plugins/hello-abos/main.js
+++ ccc-plugins/hello-abos/main.js
@@ -1,37 +1,79 @@
 'use strict';
 
-const Fs = require('fs')
+const fs       = require('fs')
+const path     = require('path')
+const encoding = 'utf8'
+const collect = []
 
+function scanSceneFires (root) {
+    fs.readdir(root, findSceneFires.bind(null, root))
+}
+
+function findSceneFires (root, err, files) {
+    if (err) throw err
+    files.map((file, index) => {
+        let filepath = [root, file].join(path.sep)
+
+        if (file.indexOf('.') === 0) {
+            return false
+        }
+        if (file.length < 5 ){
+            return false
+        }
+        if (file.lastIndexOf('.fire') === (file.length-5)) {
+            fs.readFile(filepath, encoding, (err, data) => {
+                let fire = {name: file, filepath, content: JSON.parse(data)}
+                collect.push(fire)
+            })
+            return true
+        }
+
+        fs.stat(filepath, (err, stats) => {
+            if (err) throw err
+            if (stats.isDirectory()) {
+                scanSceneFires(filepath)
+            }
+        })
+        return false
+    })
+}
 
 module.exports = {
-  load () {
-    // 当 package 被正确加载的时候执行
-  },
+    load () {
+        // 当 package 被正确加载的时候执行
+    },
 
-  unload () {
-    // 当 package 被正确卸载的时候执行
-  },
+    unload () {
+        // 当 package 被正确卸载的时候执行
+    },
 
-  messages: {
-    sayHello () {
-      Editor.log("Hello Abos, aren't u awesome!");
+    messages: {
+        refreshSceneFires(event) {
+            let root = [Editor.projectInfo.path, 'assets'].join(path.sep)
+            collect.splice(0, collect.length)
+            scanSceneFires(root)
+            Editor.log(event)
+            Editor.Ipc.sendToPanel('hello-abos', 'hehe', JSON.stringify(collect))
+        },
+        sayHello () {
+            Editor.log("Hello Abos, aren't u awesome!");
+        },
+        openBindingTreeView () {
+            Editor.Panel.open('hello-abos')
+        },
+        parseFire(event, path, third) {
+            Editor.log(third)
+            if (! fs.existsSync(path)) {
+                    Editor.log(['Invalid fire file: ', path].join(''))
+            }
+            let fire = fs.readFileSync(path)
+            fire = JSON.parse(fire)
+            if (! fire) {
+                    Editor.log(fire, path)
+            }
+            Editor.log("compile success")
+            Editor.log(fire)
+            return fire
+        }
     },
-    logging (msg) {
-      Editor.log(msg);
-    },
-    openBindingTreeView () {
-      Editor.Panel.open('hello-abos')
-    },
-    parseFire(path) {
-      if (! Fs.existsSync(path)) {
-          Editor.warning('Invalid fire file')
-      }
-      let fire = Fs.readFileSync(path)
-      fire = JSON.parse(fire)
-      if (! fire) {
-          Editor.log(fire, path)
-      }
-      return fire
-    }
-  },
 };
--- ccc-plugins/hello-abos/panel/index.js
+++ ccc-plugins/hello-abos/panel/index.js
@@ -1,37 +1,129 @@
 // panel/index.js
 
-const Fs = require('fs')
+const fs = require('fs')
 const packageName = 'hello-abos'
+const encoding = 'utf8'
 const PATH = {
     html: Editor.url('packages://hello-abos/panel/index.html'),
+    nodeTemplate: Editor.url('packages://hello-abos/panel/node-template.html'),
     style: Editor.url('packages://hello-abos/panel/less.css')
 }
 
 
-
 Editor.Panel.extend({
-    style: Fs.readFileSync(PATH.style),
-    template: Fs.readFileSync(PATH.html),
+    style: fs.readFileSync(PATH.style, encoding),
+    template: fs.readFileSync(PATH.html, encoding),
+    customProps: {
+        nodeTemplate: fs.readFileSync(PATH.nodeTemplate, encoding)
+    },
     ready () {
-        new window.Vue({
-            el: this.shadowRoot,
-            data: {
-                source: {
-                    fire: 'Game'
-                },
-                items: [
-                    {name: 'heyhey'},
-                    {name: 'yoxi'},
-                ]
+        console.log('panel ready')
+        Vue.component('node', {
+            template: this.customProps.nodeTemplate,
+            props: {
+                hierarchy: Object,
+            },
+            data () {
+                return {
+                    open: false
+                }
+            },
+            init () {
+                // console.log('compo inited', this.hierarchy.name)
+                // if (! this.hierarchy.id) {
+                //     return
+                // }
+                // Editor.Ipc.sendToPanel('scene', 'scene:query-node-info', 'this.hierarchy.id', 'cc.Node', (error, info) => {
+                //     if (error)
+                //         return Editor.error(error);
+                //     // info
+                //     console.log(info)
+                // });
+            },
+            computed: {
+                isFolder () {
+                    return this.hierarchy.children && this.hierarchy.children.length
+                }
             },
             methods: {
+                toggle () {
+                    if (this.isFolder) {
+                        this.open = !this.open
+                    }
+                },
+                displayBindding () {
+                    if (!this.hierarchy || this.hierarchy.info) {
+                        return
+                    }
+                    if (!this.hierarchy.id) {
+                        return
+                    }
+                    let self = this
+                    Editor.Ipc.sendToPanel('scene', 'scene:query-node-info', this.hierarchy.id, 'cc.Node', (error, info) => {
+                        if (error)
+                            return Editor.error(error);
+                        // info
+                        self.hierarchy.info = info
+                        console.log(info)
+                    });
+
+                    Editor.Ipc.sendToPanel('scene', 'scene:query-node', this.hierarchy.id, (error, dump) => {
+                        if (error)
+                            return Editor.error(error);
+                        node = JSON.parse(dump);
+                        self.hierarchy.node = node
+                        console.log(node)
+                    });
+
+                },
+                getFireList () {
+                    return this.source.fires
+                },
                 parseFire() {
-                    let path = [Editor.projectInfo.path, '/', this.source.fire,'.fire'].join('')
-                    let fire = Editor.Ipc.sendToMain('hello-abos:parseFire', path)
-                    console.log(fire)
+                    let path = [Editor.projectInfo.path, '/assets/', this.source.fire,'.fire'].join('')
+                    let fire = Editor.Ipc.sendToMain('hello-abos:refreshSceneFires', path)
+                },
+                sendToMain(index) {
+                    let message = this.Ipc.messages[index].name
+                    message     = ['hello-abos', message].join(':')
+                    let rsp = Editor.Ipc.sendToMain(message, this.source, ...arguments)
+                    Editor.log(rsp)
                 }
             }
         })
+        const vform = new window.Vue({
+            el: this.shadowRoot,
+            data: {
+                hierarchy: {
+                    name: 'top',
+                    children: null,
+                },
+                initManually () {
+                    console.log('inited')
+                    let self = this
+                    Editor.Ipc.sendToPanel('scene', 'scene:query-hierarchy', (error, sceneID, hierarchy) => {
+                        if (error)
+                            return Editor.error(error);
+                        // hierarchy
+                        self.hierarchy.children = hierarchy
+                        console.log(self.hierarchy)
+                    });
+                },
+                source: {
+                    fire: 'PlayGame',
+                    fires: []
+                },
+                Ipc: {
+                    messages : [
+                        {name: 'refreshSceneFires'}
+                    ],
+                }
+            },
+        })
+        window.exp = {
+            vform
+        }
+        vform.initManually()
 //   this.$btn.addEventListener('confirm', () => {
 //     Editor.Ipc.sendToMain('simple-package:say-hello', 'Hello, this is simple panel');
 //   });
