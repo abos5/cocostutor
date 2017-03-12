c5aa32fd9d51f6894f5701f40db42b4839b4e483
abosfreeman <abos5@outlook.com>
2017-03-12 23:29:44 +0800

    1. 处理坐标系问题 2. 暂时搁置不同屏幕相同显示范围的坐标系处理 3. 先处理战斗系统与用户系统

--- DragonPlanetIIDungeonRebellion/assets/scripts/battlefield.js
+++ DragonPlanetIIDungeonRebellion/assets/scripts/battlefield.js
@@ -23,35 +23,95 @@ cc.Class({
         heroPrefab: {
             type: cc.Prefab,
             default: null,
-        }
+        },
+        speedX: 5,
+        speedY: 5,
     },
 
     // use this for initialization
-    onLoad: function () {
-        console.log(this)
-        let heroNode = cc.instantiate(this.heroPrefab)
-        let pos      = heroNode.getPosition()
-        let follow   = cc.follow(heroNode)
+    onLoad () {
+        cc.gl.game = this
+        let heroNode = cc.instantiate(this.heroPrefab),
+             pos     = heroNode.getPosition(),
+             follow  = cc.follow(heroNode)
 
+        this._minX = 60
+        this._maxX = 480
+        this._maxY = 720
+        this._minY = 240
         this._heroNode = heroNode
+        this._autoMove = false
+        // this.node.scaleX = cc.game.canvas.width / this.cameraCanvas.node.width
+        // this.node.scaleY = cc.game.canvas.height / this.cameraCanvas.node.height
         this.mainstreetNode.addChild(heroNode)
-        heroNode.setPosition(this.node.getPosition())
+
         heroNode.enable = 1
-        this.cameraCanvas.node.runAction(follow)
-        this.cameraCanvas.node.setPosition(pos)
-        console.log(pos, follow)
+        heroNode.setPosition(cc.p(0, 0))
+    },
+
+    moveArround () {
+        this.moveToLeftBottom()
+        this.moveToLeftTop()
+        this.moveToRightBottom()
+        this.moveToRightTop()
+    },
+
+    moveToLeftBottom () {
+        this._move(1, cc.p(this._minX, this._minY))
+    },
+
+    moveToLeftTop () {
+        this._move(1, cc.p(this._minX, this._maxY))
+    },
+
+    moveToRightBottom () {
+        this._move(1, cc.p(this._maxX, this._minY))
+    },
+
+    moveToRightTop () {
+        this._move(1, cc.p(this._maxX, this._maxY))
+    },
+
+    _move (duration, pos) {
+        let action = cc.moveTo(duration, pos).easing(cc.easeCubicActionOut())
+        console.log(action)
+        // this.node.stopAllActions()
+        this.node.runAction(cc.sequence(action))
     },
 
     // called every frame, uncomment this function to activate update callback
-    update: function (dt) {
+    update (dt) {
+
+        if (! this._autoMove) {
+            return
+        }
+
+        let pos = this.node.getPosition()
+
+        if (pos.x+this.speedX > this._maxX) {
+            this.speedX = Math.abs(this.speedX) * -1
+        } else if (pos.x+this.speedX < this._minX) {
+            this.speedX = Math.abs(this.speedX) * 1
+        }
+
+
+        if (pos.y+this.speedY > this._maxY) {
+            this.speedY = Math.abs(this.speedY) * -1
+        } else if (pos.y+this.speedY < this._minY) {
+            this.speedY = Math.abs(this.speedY) * 1
+        }
+
+        let newPos = cc.p(
+            pos.x+this.speedX,
+            pos.y+this.speedY
+        )
+        this.node.setPosition(newPos)
+
         // let newPos = cc.p(this.node.x + this.speed, this.node.y + this.speed)
         // let newPos = cc.p(this.node.x - this.speed, this.node.y - this.speed)
         // this.node.setPosition(newPos)
         // this.node.convertToWorldSpaceAr;
         // cc.log(this.node.x, this.speed, newPos)
-        let pos = this._heroNode.getPosition()
-        let newPos = cc.p(pos.x - 1, pos.y - 1)
-        // console.log(pos, newPos)
-        this._heroNode.setPosition(newPos)
+
     },
 });
